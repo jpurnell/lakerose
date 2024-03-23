96a246637fbf5e38caf2e6023639f062ab30bf4f
Justin Purnell <jpurnell@users.noreply.github.com>
2024-03-23 09:50:20 -0400

    Initial Commit

---
+++ Lake Rose/Lake_RoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lake_RoseApp.swift
+//  Lake Rose
+//
+//  Created by Justin Purnell on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Lake_RoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
